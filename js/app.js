70b96517bab1b03ad95b778148de686e36f2ef7f
zainabdhaif <108354207+zainabdhaif@users.noreply.github.com>
2024-07-08 10:39:42 +0300

    selectors to select element and capture input are working

--- js/app.js
+++ js/app.js
@@ -3,3 +3,36 @@
 //step3: add the visual alphabets for inputs
 //step4: somehow make the letter-boxes accept and show the input from the alphabets clicked
 //
+
+const keys = document.querySelectorAll(".letter-key");
+const letterBox = document.querySelectorAll(".letter-box");
+let keyValue ='';
+let letterBoxValue = '';
+
+
+
+keys.forEach((key) =>{
+    key.addEventListener ("click", event =>{
+        if (event.target.className === "letter-key"){
+            console.log("alphabet key is clicked");
+            keyValue = event.target.innerHTML;
+            console.log(keyValue);
+    
+        }
+    });
+});
+
+
+letterBox.forEach((LB) => {
+    LB.addEventListener ("click", event =>{
+        if (event.target.className === "letter-box"){
+            console.log("letter box is clicked");
+            letterBoxValue = event.target.innerHTML;
+            console.log(letterBoxValue);
+    
+        }
+    });
+    
+})
+
+
