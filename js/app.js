96c25ad8b898a1d286a5545f6bbdb200d54dd780
zainabdhaif <108354207+zainabdhaif@users.noreply.github.com>
2024-07-08 10:16:33 +0300

    added alphabet keyboard

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//step1: add a words array to store all the possible words
+//step2: have a banned words array that includes all the words that cannot be entered
+//step3: add the visual alphabets for inputs
+//step4: somehow make the letter-boxes accept and show the input from the alphabets clicked
+//
